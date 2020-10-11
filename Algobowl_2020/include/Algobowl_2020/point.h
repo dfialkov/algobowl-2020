8f2c78551eaa09da427b6ec387397cd7d378d41b
Chris M <chris.muller@outlook.com>
2020-10-11 19:03:22 +0000

    Implemented reading input files

--- Algobowl_2020/include/Algobowl_2020/point.h
+++ Algobowl_2020/include/Algobowl_2020/point.h
@@ -1,12 +1,19 @@
+/*
+ * Authors: Chris Muller
+ */
+
 #ifndef POINT_H
 #define POINT_H
 
 struct Point {
     int x, y, z;
 
-    //create new point
-    Point(int x, int y, int z); 
-    
+    //constructor
+    Point(int x, int y, int z);
+
+    //compare 2 points (for the sake of priority_queue)
+    static bool compare(Point p1, Point p2);
+
     //calculate manhattan distance between p1 and p2
     static int calc_manhat_dist(Point p1, Point p2);
 };
--- Algobowl_2020/src/main.cpp
+++ Algobowl_2020/src/main.cpp
@@ -1,9 +1,64 @@
+/*
+ * Authors: Chris Muller, Daniel Fialkov, Thomas Deisz, Charles Gougenheim
+ */
+
 #include <iostream>
+#include <queue>
+#include <fstream>
+#include <functional>
 #include <Algobowl_2020/point.h>
 
 using namespace std;
 
 int main() {
-    Point p(1, 2, 3);
-    cout << "The points z value is: " << p.z << endl;
+    priority_queue<Point, std::vector<Point>, std::function<bool(Point, Point)>> points(Point::compare);
+    int num_points, num_sets;
+    ifstream ifs;
+
+    //access file
+    ifs.open("input.txt", ios::in);
+    if(!ifs.is_open()) {
+        cout << "Could not open file" << endl;
+        exit(1);
+    }
+
+    //read in number of points
+    if(!(ifs >> num_points)) {
+        cout << "File Format Error" << endl;
+        exit(1);
+    }
+
+    //read in number of sets
+    if(!(ifs >> num_sets)) {
+        cout << "File Format Error" << endl;
+        exit(1);
+    }
+
+    //read in n points
+    for(int i = 0; i < num_points; i++) {
+        int x, y, z;
+
+        if(!(ifs >> x)) {
+            cout << "File Format Error" << endl;
+            exit(1);
+        }
+        if(!(ifs >> y)) {
+            cout << "File Format Error" << endl;
+            exit(1);
+        }
+        if(!(ifs >> z)) {
+            cout << "File Format Error" << endl;
+            exit(1);
+        }
+
+        //emplace combines push() and Point's constructor
+        points.emplace(x, y, z);
+    }
+
+    ifs.close();
+
+    //debugging
+    cout << "number of points: " << num_points << endl;
+    cout << "number of sets: " << num_sets << endl;
+    cout << "top point x: " << points.top().x << endl;
 }
